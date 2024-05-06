2a339225fff3f6689918d8183d0643a9297d2817
Byron2016 <bgva2005@yahoo.com>
2024-05-06 09:09:51 -0500

    add new c# console project

---
+++ 074_CSharp_WPF_Events_00_Solution/074_CSharp_WPF_Events_00/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _074_CSharp_WPF_Events_00
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
