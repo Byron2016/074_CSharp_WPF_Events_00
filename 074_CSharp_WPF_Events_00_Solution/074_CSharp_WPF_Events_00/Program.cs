bcf53f0e29dbf8105f15193450528cc7fd21f99c
Byron2016 <bgva2005@yahoo.com>
2024-05-06 09:16:55 -0500

    add new delegate definition

--- 074_CSharp_WPF_Events_00_Solution/074_CSharp_WPF_Events_00/Program.cs
+++ 074_CSharp_WPF_Events_00_Solution/074_CSharp_WPF_Events_00/Program.cs
@@ -1,5 +1,6 @@
 ﻿namespace _074_CSharp_WPF_Events_00
 {
+    public delegate void ManejadorEventos<T>(Object source, T datos);
     internal class Program
     {
         static void Main(string[] args)
